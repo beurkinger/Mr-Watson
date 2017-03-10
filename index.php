1cac5b5d6d71eef513d2c9058464b32fc106151e
beurkinger <macnuggets@gmail.com>
2017-03-10 01:38:09 +0100

    query parsing added

--- index.php
+++ index.php
@@ -3,9 +3,15 @@ require 'app/config.php';
 require 'vendor/autoload.php';
 use Beurkinger\MrWatson\Watson;
 
-$watson = new Watson(USERNAME, PASSWORD);
-$response = $watson->request("Hello, my name is Thibault'. I'm very happy to be with you today. I hope you are having a great time.");
-
-http_response_code($response['statusCode']);
 header('Content-Type: application/json');
-echo $response['content'];
+
+if (isset($_GET["text"]) && $_GET['text']) {
+    $text = htmlspecialchars(trim($_GET['text']));
+    $watson = new Watson(USERNAME, PASSWORD);
+    $response = $watson->request($text);
+    http_response_code($response['statusCode']);
+    echo $response['content'];
+} else {
+    http_response_code(400);
+    echo json_encode(['error' => 'text query is not defined']);
+}
